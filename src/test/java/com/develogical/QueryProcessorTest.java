9bae0d1863a2947230f74e8fa6585e10d04af5b6
jon <jonathon.schofield@thetrainline.com>
2012-07-01 16:48:30 +0100

    Largest number test

--- src/test/java/com/develogical/QueryProcessorTest.java
+++ src/test/java/com/develogical/QueryProcessorTest.java
@@ -24,4 +24,11 @@ public class QueryProcessorTest {
     public void knowsAboutPlus() throws Exception {
         assertThat(queryProcessor.process("f7b1b440: what is 124 plus 201"), is("325"));
     }
+
+    @Test
+    public void findTheLargestNumber()
+    {
+        assertThat(queryProcessor.process("c9bcecd0: which of the following numbers is the largest: 60, 65, 978, 695"),
+                containsString("978"));
+    }
 }
