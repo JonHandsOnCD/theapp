4e512c190fdb8cd00ecb0fd94e1972f031a9a499
jon <jonathon.schofield@thetrainline.com>
2012-07-01 14:22:17 +0100

    Fixed test

--- src/test/java/com/develogical/QueryProcessorTest.java
+++ src/test/java/com/develogical/QueryProcessorTest.java
@@ -12,7 +12,7 @@ public class QueryProcessorTest {
 
     @Test
     public void returnsEmptyStringIfCannotProcessQuery() throws Exception {
-        assertThat(queryProcessor.process("test"), is(""));
+        assertThat(queryProcessor.process("test"), is("I have no knowledge of that subject"));
     }
 
     @Test
