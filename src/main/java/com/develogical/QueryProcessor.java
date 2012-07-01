c122c416155b52cdbe792db1ec41844c161d11ff
Attila Sztupak <attila.sztupak@gmail.com>
2012-07-01 14:11:51 +0100

    SPA added

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -5,6 +5,8 @@ public class QueryProcessor {
     public String process(String query) {
         if (query.contains("SPA2012")) {
             return "SPA is a conference";
+        } else if (query.contentEquals("SPA")) {
+            return "watery stuff";
         }
         return "I have no knowledge of that subject";
     }
